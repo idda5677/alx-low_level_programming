3a290b74d2d4f46ba7d42a4433817c9ae8921e1f
iddaonguta <idda.onguta@students.jkuat.ac.ke>
2023-07-17 13:43:29 -0700

    ./5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This program prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+    int number;
+
+    for (number = 0; number < 10; number++)
+    {
+        putchar(number + '0');
+    }
+
+    putchar('\n');
+
+    return 0;
+}
+
