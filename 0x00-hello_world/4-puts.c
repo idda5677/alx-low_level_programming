d418fea5b0456192476e144da7d66428c4962031
iddaonguta <idda.onguta@students.jkuat.ac.ke>
2023-07-17 04:55:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h> 
 /**
- * Description main .prints out a srtring to stdout
- * Return:0 if success
+ * Description: main .prints out a srtring to stdout.
+ * Return:0 if success.
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle.");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
 
