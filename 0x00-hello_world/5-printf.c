f744722100e6b9ef0d8b43dff167a875c6938286
iddaonguta <idda.onguta@students.jkuat.ac.ke>
2023-07-17 05:01:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message with proper grammar.
+ *              The outcome is a piece of art, followed by a new line.
+ * Return: Always 0 (indicating success)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
+
