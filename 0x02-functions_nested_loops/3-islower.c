d0d2d17cdd7e5f9fe0125f6952bb7f0279ef4bf9
iddaonguta <idda.onguta@students.jkuat.ac.ke>
2023-07-18 14:20:23 -0700

    ./3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+/**
+ * _islower - Check if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+    return (c >= 'a' && c <= 'z') ? 1 : 0;
+}
+
