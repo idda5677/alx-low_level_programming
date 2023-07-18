dc6833d7fb34ad852d98fedfaedcd33e33e3f961
iddaonguta <idda.onguta@students.jkuat.ac.ke>
2023-07-18 12:43:16 -0700

    readdme file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description: This function prints "_putchar" followed by a new line
+ *              using the _putchar function and returns 0.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	
+         _putchar("_");
+	 _putchar(
+
