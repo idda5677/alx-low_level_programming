72317aa2ff6cfb2594b84d6cefb7d50bf0b4ca72
iddaonguta <idda.onguta@students.jkuat.ac.ke>
2023-07-18 14:47:40 -0700

    ./7-last_digit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @x: The integer for which absolute value is to be computed.
+ *
+ * Return: The absolute value of x.
+ */
+int _abs(int x)
+{
+    if (x < 0)
+    {
+        return -x;
+    }
+    else
+    {
+        return x;
+    }
+}
+
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @num: The number to extract the last digit from.
+ *
+ * Return: The value of the last digit.
+ */
+int print_last_digit(int num)
+{
+    int last_digit;
+
+    last_digit = num % 10;
+
+    if (last_digit < 0)
+        last_digit = -last_digit;
+
+    _putchar(last_digit + '0');
+
+    return last_digit;
+}
+
