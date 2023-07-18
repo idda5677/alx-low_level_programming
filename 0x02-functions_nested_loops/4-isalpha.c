0817cbc64c06a2e5713ce3f500971d3d17f65929
iddaonguta <idda.onguta@students.jkuat.ac.ke>
2023-07-18 14:23:32 -0700

    ./4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+/**
+ * _isalpha - Check if a character is an alphabetic character.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise.
+ */
+int _isalpha(int c)
+{
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) ? 1 : 0;
+}
+
