3039c5c5cca7059adfbe603d09683f5cb3df31fd
iddaonguta <idda.onguta@students.jkuat.ac.ke>
2023-07-18 13:57:13 -0700

    ./0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point of the program
- * Description: This function prints "_putchar" followed by a new line
- *              using the _putchar function and returns 0.
- * Return: 0 (Success)
+ * Description: This function prints "_putchar" followed by a new line.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	
-         _putchar("_");
-	 _putchar(
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return 0;
+}
 
