6657ac71a6834496a2f4fde2c63e5d473c9aa7ce
iddaonguta <idda.onguta@students.jkuat.ac.ke>
2023-07-18 14:12:33 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line.
+ *
+ *Return void
+ */
+void print_alphabet(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        _putchar(letter);
+        letter++;
+    }
+    _putchar('\n');
+}
+
